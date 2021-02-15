1c761d9304ce58c384b89409f14c2ab8f6ba22e9
Jpa002 <%dMbkUgvh2$#!o>
2021-02-15 07:29:41 -0600

    https://github.com/Jpa002/tech-jobs-2.git

--- src/org/launchcode/techjobs_oo/PositionType.java
+++ src/org/launchcode/techjobs_oo/PositionType.java
@@ -16,7 +16,9 @@ public class PositionType {
     }
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
-
+    public String toString (String value, int id){
+        return value;
+    }
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
 
